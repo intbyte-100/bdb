26d21ae3756f9603f240dfa64fc5d80c38f0b20b
intbyte-100 <5656ltl@gmail.com>
2021-06-25 23:53:36 +0300

    add macros to define bdb objects

--- main.cpp
+++ main.cpp
@@ -4,41 +4,22 @@
 #include "src/bdb/data_struct/TypedMap.h"
 
 
+bdbClass(Entity,
+    bdbBytes(x, y, z)
+    bdbShorts(d,e,f)
+    bdbReg(regShorts(), regBytes())
+)
+
+
 int main() {
 
     bdb::DataBuffer buffer;
 
-    auto entity = new bdb::ObjectDeclaration();
-    auto entity2 = new bdb::ObjectDeclaration();
-    byte i = entity2->regLong();
-    byte i3 = entity2->regLong();
-    entity->regFloat();
-    entity->regObject(entity2, false);
-    entity->regObject(entity2, true);
+    Entity::reg();
 
-
-    entity->reg();
-    entity2->reg();
-
-
-    bdb::update();
-
-    auto instance = entity->newInstance();
-    auto bytes = buffer.write(instance);
-    buffer.read(bytes, entity->reference);
-    std::cout << "entity size: " << entity->size << "\n" << "buffer size: " << bytes.size() << "\n" << "total size: " << entity->size + entity->manifestSize << "\n";
-
-    for(auto i: bytes)
-        std::cout << (int) i << " ";
-
-    std::cout << std::endl;
-
-
-    std::vector<byte> buffer2;
-    bdb::buffer::put(5, &buffer2);
-    bdb::buffer::put(58, &buffer2);
-
-    delete entity;
-    delete instance;
-    return 0;
+    auto obj = Entity::object->newInstance();
+    Entity::x(obj) = 100;
+    Entity::y(obj) = 100;
+    Entity::z(obj) = 100;
+    buffer.write(obj);
 }
--- src/bdb/bdb.h
+++ src/bdb/bdb.h
@@ -1,11 +1,47 @@
 #pragma once
 
+#include "util/macros_foreach.h"
 #include <cstdint>
+
+
+#define bdbClass(name, code) namespace name { bdb::ObjectDeclaration *object = new bdb::ObjectDeclaration(); code}
+#define bdbVar(name) byte name;
+#define DEFINE(macro) macro;
+
+#define bdbReg(...) void reg() { \
+    object->reg();               \
+    FOR_EACH(DEFINE, __VA_ARGS__)\
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+#define bdbByteReg(name) name = object->regByte();
+#define bdbByteGetter(name) signed_byte& name(bdb::ObjectInstance* instance) {\
+    return instance->bytes[variables::name];                                  \
+}
+#define bdbBytes(...) namespace variables { FOR_EACH(bdbVar, __VA_ARGS__) } \
+void regBytes() {                                                           \
+    using namespace variables;                                              \
+    FOR_EACH(bdbByteReg, __VA_ARGS__)                                       \
+}                                                                           \
+FOR_EACH(bdbByteGetter, __VA_ARGS__)
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+#define bdbShortReg(name) name = object->regShort();
+#define bdbShortGetter(name) short& name(bdb::ObjectInstance* instance) {      \
+    return instance->shorts[variables::name];                                  \
+}
+#define bdbShorts(...) namespace variables { FOR_EACH(bdbVar, __VA_ARGS__) } \
+void regShorts() {                                                           \
+    using namespace variables;                                               \
+    FOR_EACH(bdbShortReg, __VA_ARGS__)                                       \
+}                                                                            \
+FOR_EACH(bdbShortGetter, __VA_ARGS__)
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 typedef uint8_t byte;
 typedef char signed_byte;
 
 namespace bdb {
+
 
     const byte version = 1;
     const byte ZIP = 0x01;
