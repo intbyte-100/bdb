6adb9eea5f67cd047a0d2267cc6b7564dc0a5168
intbyte-100 <5656ltl@gmail.com>
2021-12-31 00:49:34 +0300

    fix a sigmentation fault in __bdbExtendList

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "stdio.h"
 #include "array.h"
+#include "list.h"
 
 int main() {
     BdbArray* array = bdbAllocArray(int, 200);
     printf("array element = %i\n", *bdbArrayElement(int, array, 0));
-
     bdbDeleteArray(array);
+
+    BdbList *list = bdbCreateList();
 }
--- src/list.c
+++ src/list.c
@@ -3,19 +3,19 @@
 
 void __bdbExtendList(BdbList* list, int elementSize, int elementsCount)
 {
-    if (!(list->size>= list->array.size+elementsCount*elementSize)){
+    if (list->size >= list->array.size + elementsCount * elementSize) {
         int newSize = list->array.size*1.60;
         __bdbResizeList(list, newSize);
         list->array.size = newSize;
         list->size += elementsCount * elementSize;
-    } else{
+    } else {
         list->size += elementsCount * elementSize;
     }
 }
 
 void __bdbResizeList(BdbList* list, int newSize)
 {
-    realloc(list->array.values, newSize);
+    list->array.values = realloc(list->array.values, newSize);
     list->array.size = newSize;
 }
 
@@ -23,6 +23,15 @@ char * __bdbAddElement(BdbList* list, int size)
 {
     __bdbExtendList(list, size, 1);
     return &list->array.values[list->array.size - size];
+}
+
+
+BdbList * bdbCreateList()
+{
+    BdbList *list = malloc(sizeof(BdbList));
+    list->array.values = malloc(14);
+    list->array.size = 14;
+    return list;
 }
 
 
